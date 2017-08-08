676b0ec718e5b2b2c0c6d7e7cb6074fe03c67830
jules maregiano <jules.maregiano@gmail.com>
2017-08-08 16:05:38 +0200

    CRUD mis en place

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(task: "Vaisselle", status: true)
+Task.create(task: "Gazon", status: false)
